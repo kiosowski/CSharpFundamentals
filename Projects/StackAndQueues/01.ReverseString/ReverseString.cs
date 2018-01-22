f5547145d2af6938a077049e9cb1c73fc57c3661
kiosowski <g.kiosovski@abv.bg>
2018-01-22 19:20:14 +0200

    ADD

---
+++ Projects/StackAndQueues/01.ReverseString/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _01.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<char>();
+
+            foreach (var char in stack)
+            {
+
+            }
+        }
+    }
+}
---
+++ Projects/StackAndQueues/02.SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var elements = input.Split(' ');
+            var stack = new Stack<string>();
+
+            for (int i = elements.Length -1; i >= 0; i--)
+            {
+                stack.Push(elements[i]);
+            }
+            while (stack.Count > 1)
+            {
+                var leftOperand = int.Parse(stack.Pop());
+                var operand = stack.Pop();
+                var rightOperand = int.Parse(stack.Pop());
+                switch (operand)
+                {
+                    case "+":
+                        stack.Push((leftOperand + rightOperand).ToString());
+                        break;
+                    case "-":
+                        stack.Push((leftOperand - rightOperand).ToString());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(stack.Pop());
+            
+
+        }
+    }
+}
