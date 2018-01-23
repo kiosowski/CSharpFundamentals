63f51cb12b9b153e29bed8377e4485f2e638bb0d
kiosowski <g.kiosovski@abv.bg>
2018-01-23 12:58:15 +0200

    Add

---
+++ Projects/StackAndQueues/04.MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.MatchingBrackets
+{
+    class MatchingBrackets
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    var startIndex = stack.Pop();
+                    var contents = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(contents);
+                }
+                
+            }
+        }
+    }
+}
---
+++ Projects/StackAndQueues/06.TrafficLight/TrafficLight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.TrafficLight
+{
+    class TrafficLight
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>();
+            var count = 0;
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    var carsCount = queue.Count;
+                    for (int i = 0; i < Math.Min(carsCount,n); i++)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+              
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
