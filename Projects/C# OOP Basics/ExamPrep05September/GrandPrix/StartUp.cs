81a17e9afad9cf301f9e4ba30f91d092ce22c982
kiosowski <g.kiosovski@abv.bg>
2018-03-15 13:47:42 +0200

    Update projects.

---
+++ Projects/C# OOP Basics/ExamPrep05September/GrandPrix/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GrandPrix
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var raceTower = new RaceTower();
+            var engine = new Engine(raceTower);
+            engine.Start();
+        }
+    }
+}
